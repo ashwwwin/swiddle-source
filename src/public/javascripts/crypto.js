b027f782714538e598f43c900bae38c5511f7277
ashwwwin <ashwindeepak9999@gmail.com>
2021-11-18 06:14:06 +0700

    Front end mint counter, working disabled mint button, cleaned up package.json

--- src/public/javascripts/crypto.js
+++ src/public/javascripts/crypto.js
@@ -1,5 +1,5 @@
-var NFT_ADDRESS = '0x6fdD6bda3b3F06187141D02523dE8be33E7bDF6f';
-var CONTRACT_ADDRESS = '0x6E1a4F1bfd4B01348696261048a6deA43FC48eA5';
+var NFT_ADDRESS = '0xB312cB8244D5DC9E929B3ab6714a30fC2dFf36B2';
+var CONTRACT_ADDRESS = '0xB426cb378437Dd4e2a53ff9DEd7BBCec9569B240';
 
 // Import the contract file
 var ogFlatsNFT;
@@ -53,9 +53,7 @@ function updateMintCounter(){
 
 $('#mint-og-flat').click(async function () {
   
-  $('#mint-og-flat').css('cursor', 'not-allowed');
-  $('#mint-og-flat').css('user-events', 'none');
-  $('#mint-og-flat').prop('disabled', true);
+  $('#mint-og-flat').css('pointer-events', 'none');
 
   //Checking if user is logged in 
   accounts = await ethereum.request({ method: 'eth_requestAccounts' });
@@ -65,29 +63,30 @@ $('#mint-og-flat').click(async function () {
       const provider = await new ethers.providers.Web3Provider(ethereum);
       const signer = await provider.getSigner();
       const connectedContract = await new ethers.Contract(CONTRACT_ADDRESS, ogFlatsNFT.abi, signer);
-
-      $('#mint-og-flat').text('Waiting for ether');
-      let nftTxn = await connectedContract.mint('0', accounts[0], { value: ethers.utils.parseEther("0.75") });
-
       console.log(connectedContract);
+
+      let nftTxn = await connectedContract.mint('0', accounts[0], { value: ethers.utils.parseEther("0.875") });
+      $('#mint-og-flat').text('Waiting for ether');
 
       $('#mint-og-flat').text('Minting...');
       await nftTxn.wait();
-
-      $('#mint-og-flat').text('Mint again');
-
       console.log(nftTxn);
 
       $('#success-address').show();
+      $('#success-address').css('cursor', 'pointer');
       $('#success-address').text(`https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
       $('#success-address').click(function() {
         window.open(`https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
       });
 
-      $('#success-address').css('cursor', 'pointer');
+      $('#mint-og-flat').text('Mint again');
+      $('#mint-og-flat').css('cursor', 'pointer');
+      $('#mint-og-flat').css('pointer-events', 'auto');
     } catch (error) {
+      $('#mint-og-flat').text('Mint');
+      $('#mint-og-flat').css('cursor', 'pointer');
+      $('#mint-og-flat').css('pointer-events', 'auto');
       console.log(error);
-      $('#metamask-signin').click();
     }
   } else {
       alert('Please login to MetaMask');
